6b98f92d5df3d4800ac11010dca6d453bd6b3ede
nishitaa70 <nishitaa70@gmail.com>
2021-12-11 15:46:52 +0530

    second commit

---
+++ Assignment 2/q1.java
@@ -0,0 +1,9 @@
+public class q1{
+    public static void main(String arg []){
+        System.out.println("  + '' '' '' '' + ");
+        System.out.println("[ |    o   o    | ] ");
+        System.out.println("  |      ^      |   ");
+        System.out.println("  |    ' _ '    |   ");
+        System.out.println("  +-------------+ ");
+    }
+}
