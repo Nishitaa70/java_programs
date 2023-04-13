4b10c0ac6aef66d2f5e1b7c047055c946ea7b3aa
nishitacse <nishita.sharma.cse.2020@miet.ac.in>
2023-04-13 19:30:29 +0530

    update

--- Basic Programs/src/com/LinkedList/Main.java
+++ Basic Programs/src/com/LinkedList/Main.java
@@ -1,4 +1,13 @@
 package com.LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(4);
+        list.insertFirst(6);
+        list.insertFirst(78);
+        list.insertFirst(23);
+        list.display();
+    }
+
 }
