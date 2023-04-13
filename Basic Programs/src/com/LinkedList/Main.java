478171f1ee5ab80820368551d96eee3fe69af5b6
nishitaa70 <71011361+nishitaa70@users.noreply.github.com>
2023-04-13 17:14:48 +0000

    LL

--- Basic Programs/src/com/LinkedList/Main.java
+++ Basic Programs/src/com/LinkedList/Main.java
@@ -5,6 +5,8 @@ public class Main {
         LL list = new LL();
         list.insertFirst(4);
         list.insertFirst(6);
+        list.insertFirst(45);
+        list.insertFirst(56);
         list.insertFirst(78);
         list.insertFirst(23);
         list.insertMiddle(67, 3);
